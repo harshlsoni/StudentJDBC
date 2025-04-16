36b774cda7a24a1dc706d986df0922d38857325a
harshlsoni <harshlsoni2920@gmail.com>
2025-04-16 19:04:23 +0530

    Student Constructor created

--- Student.java
+++ Student.java
@@ -4,4 +4,14 @@ public class Student {
     private String name;
     private String dob;
     private double marks;
+
+    // Constructor
+    public Student(String prn, String name, String dob, double marks) {
+        this.prn = prn;
+        this.name = name;
+        this.dob = dob;
+        this.marks = marks;
+    }
+
+	   
 }
