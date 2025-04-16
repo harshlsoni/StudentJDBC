5a122f444c6c87c155998ff4848f3f3e170d57f1
harshlsoni <harshlsoni2920@gmail.com>
2025-04-16 19:04:48 +0530

    Getters setters methods created

--- Student.java
+++ Student.java
@@ -13,5 +13,19 @@ public class Student {
         this.marks = marks;
     }
 
-	   
+	    // Getters
+		public String getPrn() { return prn; }
+		public String getName() { return name; }
+		public String getDob() { return dob; }
+		public double getMarks() { return marks; }
+
+		// Setters
+		public void setName(String name) { this.name = name; }
+		public void setDob(String dob) { this.dob = dob; }
+		public void setMarks(double marks) { this.marks = marks; }
+	
+	
+
+
+
 }
